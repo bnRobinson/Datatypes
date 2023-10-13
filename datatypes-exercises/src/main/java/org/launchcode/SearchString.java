6a706f9da372367550264cff9636485662315e17
Brittany Robinson <bncopple@gmail.com>
2023-10-12 21:58:10 -0500

    Strings exercises done

--- datatypes-exercises/src/main/java/org/launchcode/SearchString.java
+++ datatypes-exercises/src/main/java/org/launchcode/SearchString.java
@@ -1,8 +1,12 @@
 package org.launchcode;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class SearchString {
-    public static void main(String[]args){
-        String aliceInWonderland= "Alice was beginning to " +
+    public static void main(String[] args) {
+        String aliceInWonderland = "Alice was beginning to " +
                 "get very tired of sitting by her " +
                 "sister on the bank, and of having " +
                 "nothing to do: once or twice she had " +
@@ -12,7 +16,19 @@ public class SearchString {
                 "use of a book,’ thought Alice ‘without " +
                 "pictures or conversation?’";
 
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter search term:");
+        String searchTerm = input.nextLine();
+        Integer length= searchTerm.length();
 
+        if(aliceInWonderland.toUpperCase().contains(searchTerm.toUpperCase())){
+            System.out.println("Term found at index: "+ aliceInWonderland.indexOf(searchTerm)+ "Term length: "+ length);
 
+            String newAlice= aliceInWonderland.replace(searchTerm, "");
+
+            System.out.println("New Sentence: "+ newAlice);
+        } else {
+            System.out.println("Term Not Found");
+        }
     }
 }
